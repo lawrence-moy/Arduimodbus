8a5bf68b5b1f957af30b7853abe088ff6f5b581f
Lawrence Moy <moylaw31@gmail.com>
2019-07-16 23:42:01 +0200

    Fix compilation error

--- src/arduimodbus.ino
+++ src/arduimodbus.ino
@@ -62,6 +62,112 @@ void configurePinMode(uint16_t pinIndex,
 
 /******************************************************************************/
 
+/**
+ * Handle Read Input Status (FC=02)
+ * write back the values from digital in pins (input status).
+ *
+ * handler functions must return void and take:
+ *      uint8_t  functionCode - modbus function code.
+ *      uint16_t address - first register/coil address.
+ *      uint16_t length/status - length of data / coil status.
+ */
+uint8_t
+readDigitalIn(uint8_t  functionCode, 
+              uint16_t address, 
+              uint16_t length) {
+    if (FC_READ_COILS == functionCode ||
+        FC_READ_DISCRETE_INPUT == functionCode) {
+      for (int coilIndex = 0; coilIndex < length; coilIndex++) {
+        slave.writeCoilToBuffer(coilIndex, digitalRead(address + coilIndex));
+      }
+      return STATUS_OK;
+    }
+    return STATUS_ILLEGAL_FUNCTION;
+}
+
+/******************************************************************************/
+
+/* Handle Force Single Coil (FC=05) and Force Multiple Coils (FC=15)
+ * set digital output pins (coils).
+ */
+uint8_t 
+writeDigitalOut(uint8_t  function, 
+                uint16_t address, 
+                uint16_t length) {
+    (void)function;
+    (void)address;
+    // set digital pin state(s).
+    for (int currentCoilIndex = 0; currentCoilIndex < length; currentCoilIndex++) {
+        digitalWrite(address + currentCoilIndex, slave.readCoilFromBuffer(currentCoilIndex));
+    }
+    return STATUS_OK;
+}
+
+/*****************************************************************************************/
+/* Handle Write Holding Register(s) (FC=06, FC=16)
+ * write data into eeprom.
+ */
+uint8_t
+writeMemory(uint8_t  function, 
+            uint16_t address, 
+            uint16_t length) {
+    (void)function;
+    uint16_t value;
+    uint16_t registerIndex;
+    
+    // write to eeprom.
+    for (int currentRegisterIndex = 0; currentRegisterIndex < length; currentRegisterIndex++) {
+        registerIndex = address + currentRegisterIndex;
+        // get uint16_t value from the request buffer.
+        value = slave.readRegisterFromBuffer(currentRegisterIndex);
+        EEPROM.put(registerIndex * 2, value);
+        configurePinMode(registerIndex * 2,  value);
+    }
+    return STATUS_OK;
+}
+
+/******************************************************************************/
+
+/* Handle Read Input Registers (FC=04)
+ * write back the values from analog in pins (input registers).
+ */
+uint8_t 
+readAnalogIn(uint16_t address, 
+             uint16_t length) {
+    for (int currentAnalogPin = 0; currentAnalogPin < length; currentAnalogPin++) {
+        // write uint16_t value to the response buffer.
+        slave.writeRegisterToBuffer(currentAnalogPin, analogRead(address + currentAnalogPin));
+    }
+}
+
+/******************************************************************************/
+
+/* Handle Read Holding Registers (FC=03)
+ * write back the values from eeprom (holding registers).
+ */
+uint8_t
+readMemory(uint8_t  function, 
+           uint16_t address, 
+           uint16_t length) {
+    if (FC_READ_INPUT_REGISTERS == function) { //function 4
+        readAnalogIn(address, length);
+        return STATUS_OK;
+    }
+    
+    if (address >= (EEPROM_SIZE/2)-1) { // SIZE/2 = convert in 16 bit length
+      return STATUS_ILLEGAL_DATA_ADDRESS;
+    }
+
+    uint16_t value;
+    for (int registerIndex = 0; registerIndex < length; registerIndex++) {
+        EEPROM.get((address + registerIndex) * 2, value);
+        slave.writeRegisterToBuffer(registerIndex, value);
+    }
+    return STATUS_OK;
+}
+
+/******************************************************************************/
+
 void setup() {
     pinMode(ADDR_CFG_PIN_0, INPUT);
     pinMode(ADDR_CFG_PIN_1, INPUT);
@@ -74,8 +180,12 @@ void setup() {
                      ((digitalRead(ADDR_CFG_PIN_3) & 0x1) << 3);
     slave.setSlaveId(modbusAddr);
 
-    for (uint16_t pinIndex = 7; pinIndex < 12; pinIndex++) {
-        configurePinMode(pinIndex, EEPROM.read(pinIndex * 2)); //2 for 16 bit registers
+    for (uint16_t pinIndex = 7; pinIndex <= 13; pinIndex++) {
+        uint16_t mode = EEPROM.read(pinIndex * 2);
+        if (PIN_MODE_OUTPUT != mode && PIN_MODE_INPUT != mode) {
+            mode = PIN_MODE_OUTPUT;
+        }
+        configurePinMode(pinIndex, mode); //2 for 16 bit registers
     }
     // RS485 control pin must be output
     pinMode(CTRL_PIN, OUTPUT);
@@ -102,101 +212,4 @@ void loop() {
 }
 
 /******************************************************************************/
-
-/**
- * Handle Read Input Status (FC=02)
- * write back the values from digital in pins (input status).
- *
- * handler functions must return void and take:
- *      uint8_t  functionCode - modbus function code.
- *      uint16_t address - first register/coil address.
- *      uint16_t length/status - length of data / coil status.
- */
-uint8_t readDigitalIn(uint8_t  functionCode, 
-              uint16_t address, 
-              uint16_t length) {
-    if (FC_READ_COILS == functionCode ||
-        FC_READ_DISCRETE_INPUT == functionCode) {
-  		for (int coilIndex = 0; coilIndex < length; coilIndex++) {
-  		    slave.writeCoilToBuffer(coilIndex, digitalRead(address + coilIndex));
-  		}
-  		return STATUS_OK;
-    }
-    return STATUS_ILLEGAL_FUNCTION;
-}
-/******************************************************************************/
-
-/* Handle Read Input Registers (FC=04)
- * write back the values from analog in pins (input registers).
- */
-uint8_t readAnalogIn(uint16_t address, 
-             uint16_t length) {
-    for (int currentAnalogPin = 0; currentAnalogPin < length; currentAnalogPin++) {
-        // write uint16_t value to the response buffer.
-        slave.writeRegisterToBuffer(currentAnalogPin, analogRead(address + currentAnalogPin));
-    }
-}
-/******************************************************************************/
-
-/* Handle Read Holding Registers (FC=03)
- * write back the values from eeprom (holding registers).
- */
-uint8_t readMemory(uint8_t  function, 
-           uint16_t address, 
-           uint16_t length) {
-    if (FC_READ_INPUT_REGISTERS == function) { //function 4
-        readAnalogIn(address, length);
-        return STATUS_OK;
-    }
-    
-    if (address >= (EEPROM_SIZE/2)-1) { // SIZE/2 = convert in 16 bit length
-		return STATUS_ILLEGAL_DATA_ADDRESS;
-	}
-
-    uint16_t value;
-    for (int registerIndex = 0; registerIndex < length; registerIndex++) {
-        EEPROM.get((address + registerIndex) * 2, value);
-        slave.writeRegisterToBuffer(registerIndex, value);
-    }
-    return STATUS_OK;
-}
-
-/******************************************************************************/
-
-/* Handle Force Single Coil (FC=05) and Force Multiple Coils (FC=15)
- * set digital output pins (coils).
- */
-uint8_t writeDigitalOut(uint8_t  function, 
-                        uint16_t address, 
-                        uint16_t length) {
-    (void)function;
-    (void)address;
-    // set digital pin state(s).
-    for (int currentCoilIndex = 0; currentCoilIndex < length; currentCoilIndex++) {
-        digitalWrite(address + currentCoilIndex, slave.readCoilFromBuffer(currentCoilIndex));
-    }
-    return STATUS_OK;
-}
-
-/*****************************************************************************************/
-/* Handle Write Holding Register(s) (FC=06, FC=16)
- * write data into eeprom.
- */
-uint8_t writeMemory(uint8_t  function, 
-            uint16_t address, 
-            uint16_t length) {
-    (void)function;
-    uint16_t value;
-    uint16_t registerIndex;
-    
-    // write to eeprom.
-    for (int currentRegisterIndex = 0; currentRegisterIndex < length; currentRegisterIndex++) {
-        registerIndex = address + currentRegisterIndex;
-        // get uint16_t value from the request buffer.
-        value = slave.readRegisterFromBuffer(currentRegisterIndex);
-        EEPROM.put(registerIndex * 2, value);
-        configurePinMode(registerIndex * 2,  value);
-    }
-    return STATUS_OK;
-}
 
